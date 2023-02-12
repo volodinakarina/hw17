e147a41c3c04ad0302848f06123ae1940e0ee2c9
volodinakarina <volodinakarina27@yandex.ru>
2023-02-12 16:55:46 +0300

    change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
-
+import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -35,6 +35,47 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+# schemas
+movie_schema = MovieSchema()
+
+# namespaces
+api = Api(app)
+movie_ns = api.namespace('movies')
+
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+        stmt = Movie.query
+        if director_id:
+            stmt = stmt.filter(Movie.director_id == director_id)
+        if genre_id:
+            stmt = stmt.filter(Movie.genre_id == genre_id)
+        movies = stmt.all()
+        return movie_schema.dump(movies, many=True), 200
+
+
+@movie_ns.route('/<int:mid>')
+class MovieView(Resource):
+    def get(self, mid: int):
+        movie = Movie.query.get(mid)
+        if not movie:
+            return '', 404
+        return movie_schema.dump(movie), 200
 
 
 if __name__ == '__main__':
